ef21e484691ebb39f91fda99efcc16b9bb641d86
Yash Verma <yash44365@gmail.com>
2025-10-04 23:07:09 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import {
   Route,
   Navigate,
   Link,
+  useNavigate
 } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
